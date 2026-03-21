50d65222f22d42885a5d2bf200b3cd7bb33ed5ac
Vermashubh646 <vermashubh646@gmail.com>
2026-03-22 00:10:02 +0530

    refactor: Relocate and update STATICFILES_DIRS to use pathlib for path construction.

--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'templates')]
 
@@ -149,3 +149,4 @@ TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'templates')]
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/login/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
